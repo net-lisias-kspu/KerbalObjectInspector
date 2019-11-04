c1c511f52960df453b8df9e28d0f7776ac1c4a25
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-11-03 23:29:58 -0500

    Updated shader name with "Legacy Shaders/"  Added checks for obsolete attributes in  Inspector.cs

--- KerbalObjectInspector/Inspector.cs
+++ KerbalObjectInspector/Inspector.cs
@@ -107,23 +107,29 @@ namespace KerbalObjectInspector
             FieldInfo[] pubFields = component.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
             for (int i = 0; i < pubFields.Length; i++)
             {
-                object val = null;
-                try { val = pubFields[i].GetValue(component); }
-                catch (Exception) { continue; }
-               GUILayout.BeginHorizontal();
-               GUILayout.Label("[Field] " + pubFields[i].Name + ": " + (val != null ? val.ToString() : "null"), HighLogic.Skin.label);
-               GUILayout.EndHorizontal();
+                if (!pubFields[i].IsDefined(typeof(ObsoleteAttribute), true))
+                {
+                    object val = null;
+                    try { val = pubFields[i].GetValue(component); }
+                    catch (Exception) { continue; }
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("[Field] " + pubFields[i].Name + ": " + (val != null ? val.ToString() : "null"), HighLogic.Skin.label);
+                    GUILayout.EndHorizontal();
+                }
             }
 
             PropertyInfo[] pubProperties = component.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
             for (int i = 0; i < pubProperties.Length; i++)
             {
-                object val = null;
-                try { val = pubProperties[i].GetValue(component, null); }
-                catch (Exception) { continue; }
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("[Property] " + pubProperties[i].Name + ": " + (val != null ? val.ToString() : "null"), HighLogic.Skin.label);
-                GUILayout.EndHorizontal();
+                if (!pubProperties[i].IsDefined(typeof(ObsoleteAttribute), true))
+                {
+                    object val = null;
+                    try { val = pubProperties[i].GetValue(component, null); }
+                    catch (Exception) { continue; }
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("[Property] " + pubProperties[i].Name + ": " + (val != null ? val.ToString() : "null"), HighLogic.Skin.label);
+                    GUILayout.EndHorizontal();
+                }
             }
 
             if (pubFields.Length == 0 && pubProperties.Length == 0)
